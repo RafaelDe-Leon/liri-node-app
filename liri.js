09e0576381cf79782c3f6b7b2167ddfaf997750a
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-09-29 01:21:13 -0400

    movies working, imdb working, do what it says working

--- liri.js
+++ liri.js
@@ -1,114 +1,180 @@
 require("dotenv").config();
 
-
 // Require the key.js file
 var keys = require("./keys.js");
 
-//Require the node Package manager for spotify, request, and moment
-var Spotify = require('node-spotify-api');
-var request = require('request');
-var moment = require('moment');
+//Require the node Package manager for spotify, moment, axios
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var axios = require("axios");
 
 //save key to a var
 var spotify = new Spotify(keys.spotify);
 
 // Include file system module
-var fs = require ('fs');
-
-
+var fs = require("fs");
 
 // Make it so liri.js can take in one of the following commands:
 
 // * `concert-this`
 
-// * `spotify-this-song`
-
-// * `movie-this`
-
 // * `do-what-it-says`
-
 
 // Grab search command line argument
 var liriCommands = process.argv[2];
 // Joining the remaining arguments since an actor or tv show name may contain spaces
 var inputSearch = process.argv.slice(3).join(" ");
 
-
-
-
-
 switch (liriCommands) {
-        case 'spotify-this-song':
-            //name of function for spotify
-            getMySpotify(inputSearch);
-            break;
+  case "spotify-this-song":
+    //name of function for spotify
+    getMySpotify(inputSearch);
+    break;
 
-        default: 
-        console.log("Liri can't do that yet.");
-    }
+  case "movie-this":
+    // name of the function for movies
+    getMyMovie(inputSearch);
+    break;
+
+  case "concert-this":
+    getMyConcert(inputSearch);
+    break;
+
+  case "do-what-it-says":
+    getWhatItSays(inputSearch);
+    break;
+
+  default:
+    console.log("Liri can't do that yet.");
+}
 
 // spotify search
 function getMySpotify(inputSearch) {
- 
-spotify.search({ type: 'track', query: inputSearch }, function(err, data) {
+  // if inputaSearch is left empty default will be "The Sign"
+  if (inputSearch === "") {
+    inputSearch = "The Sign";
+  }
+
+  spotify.search({ type: "track", query: inputSearch }, function(err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      return console.log("Error occurred: " + err);
     }
-   
-  console.log(data.tracks.items[0]); 
 
-var songs = data.tracks.items;
-for (var i = 0; i < songs.length; i++) {
+    console.log(data.tracks.items[0]);
 
-    var albumObject = songs[i].album;
-    var trackName = songs[i].name;
-    var preview = songs[i].preview_url
-    
-    //store artists array to vars
-    // var artistsInfo = albumObject.artists;
-        console.log(i);
-        console.log('\nartist(s): ' + albumObject.name);
-        console.log('\nsong name: ' + trackName);
-        console.log('\npreview song: ' + preview);
-        console.log('\nalbum: ' + albumObject.name);
-        console.log('\n');
-    
+    var songs = data.tracks.items;
+    for (var i = 0; i < songs.length; i++) {
+      var albumObject = songs[i].album;
+      var trackName = songs[i].name;
+      var preview = songs[i].preview_url;
 
-}
+      //store artists array to vars
+      // var artistsInfo = albumObject.artists;
+      console.log(i);
+      console.log("\nartist(s): " + albumObject.name);
+      console.log("\nsong name: " + trackName);
+      console.log("\npreview song: " + preview);
+      console.log("\nalbum: " + albumObject.name);
+      console.log("\n");
+    }
   });
 }
 
+// movie function api
+function getMyMovie(inputSearch) {
+  // if inputSearch is left empty default movie will be "lord of the rings".
+  if (inputSearch === "") {
+    inputSearch = "Lord of the rings";
+  }
+
+  axios
+    .get(
+      "http://www.omdbapi.com/?t=" +
+        inputSearch +
+        "&y=&plot=short&apikey=trilogy"
+    )
+    .then(function(response) {
+      // console.log(response.data);
+
+      var movieObject = response.data;
+      var movieTitle = response.data.Title;
+      var yearReleased = movieObject.Released;
+      var imbRatings = movieObject.imdbRating;
+      var country = movieObject.Country;
+      var language = movieObject.Language;
+      var plot = movieObject.Plot;
+      var actors = movieObject.Actors;
+
+      console.log("\nMovie Title: " + movieTitle);
+      console.log("\nYear of Release: " + yearReleased);
+      console.log("\nIMDB Rating: " + imbRatings);
+      console.log("\nCountry: " + country);
+      console.log("\nLanguage: " + language);
+      console.log("\nPlot: " + plot);
+      console.log("\nActors: " + actors);
+    })
+    .catch(function(err) {
+      if (err) {
+        console.log("---------------Data---------------");
+        console.log(err.response.data);
+        console.log("---------------Status---------------");
+        console.log(err.response.status);
+        console.log("---------------Status---------------");
+        console.log(err.response.headers);
+      } else if (err.request) {
+        console.log(err.request);
+      } else {
+        console.log("Error", err.message);
+      }
+    });
+}
+
+//concert function api
+
+function getMyConcert() {
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        inputSearch +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(err) {
+      if (err) {
+        console.log("---------------Data---------------");
+        console.log(err.response);
+        console.log("---------------Status---------------");
+        console.log(err.response.status);
+        console.log("---------------Status---------------");
+        console.log(err.response.headers);
+      } else if (err.request) {
+        console.log(err.request);
+      } else {
+        console.log("Error", err.message);
+      }
+    });
+}
 
 
-//---------------------------------------------
+function getWhatItSays() {
+    
+fs.readFile("random.txt", "utf8", function (error, data) {
+    
+    if (error) {
+        return console.log(error);
+    }
 
-// //Assign data being used to a variable
-// var info = data.tracks.items
-// // console.log(info);
+    var dataArr = data.split(",");
 
-// //Loop through all the "items" array
-// for (var i = 0; i < info.length; i++) {
-//     //Store "album" object to variable
-//     var albumObject = info[i].album;
-//     var trackName = info[i].name
-//     var preview = info[i].preview_url
-//     //Store "artists" array to variable
-//     var artistsInfo = albumObject.artists
-//     //Loop through "artists" array
-//     for (var j = 0; j < artistsInfo.length; j++) {
-//         console.log("Artist: " + artistsInfo[j].name)
-//         console.log("Song Name: " + trackName)
-//         console.log("Preview of Song: " + preview)
-//         console.log("Album Name: " + albumObject.name)
-//         console.log("----------------")
-//     }
-// }
-// })
+    getMySpotify(dataArr[1]);
 
+    // console.log(dataArr);
 
-// -----------------------------------------
+})
 
-
+}
 // var runThis = function(argOne, argTwo) {
 //     pick(argOne, argTwo)
 // };
