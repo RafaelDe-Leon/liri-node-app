6d39898761142d90a7b303119c073581a85a60a9
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-09-28 20:20:19 -0400

    spotify is working

--- liri.js
+++ liri.js
@@ -1,9 +1,19 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
 
+// Require the key.js file
+var keys = require("./keys.js");
+
+//Require the node Package manager for spotify, request, and moment
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var moment = require('moment');
+
+//save key to a var
 var spotify = new Spotify(keys.spotify);
+
+// Include file system module
+var fs = require ('fs');
 
 
 
@@ -18,31 +28,34 @@ var spotify = new Spotify(keys.spotify);
 // * `do-what-it-says`
 
 
-var pick = function (caseData, functionData) {
-    switch (caseData) {
+// Grab search command line argument
+var liriCommands = process.argv[2];
+// Joining the remaining arguments since an actor or tv show name may contain spaces
+var inputSearch = process.argv.slice(3).join(" ");
+
+
+
+
+
+switch (liriCommands) {
         case 'spotify-this-song':
             //name of function for spotify
-            getMySpotify(functionData);
+            getMySpotify(inputSearch);
             break;
 
         default: 
         console.log("Liri can't do that yet.");
     }
-}
-
-var getArtistName = function(artist) {
-    return artist.name
-}
 
 // spotify search
-var getMySpotify = function(songName) {
+function getMySpotify(inputSearch) {
  
-spotify.search({ type: 'track', query: songName }, function(err, data) {
+spotify.search({ type: 'track', query: inputSearch }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
    
-//   console.log(data.tracks.items[0]); 
+  console.log(data.tracks.items[0]); 
 
 var songs = data.tracks.items;
 for (var i = 0; i < songs.length; i++) {
@@ -52,19 +65,15 @@ for (var i = 0; i < songs.length; i++) {
     var preview = songs[i].preview_url
     
     //store artists array to vars
+    // var artistsInfo = albumObject.artists;
+        console.log(i);
+        console.log('\nartist(s): ' + albumObject.name);
+        console.log('\nsong name: ' + trackName);
+        console.log('\npreview song: ' + preview);
+        console.log('\nalbum: ' + albumObject.name);
+        console.log('\n');
+    
 
-    var artistsInfo = albumObject.artists;
-
-    for (var j = 0; j < artistsInfo.length; j++) {
-
-    }
-
-    console.log(i);
-    console.log('\nartist(s): ' + songs[i].artist);
-    console.log('\nsong name: ' + songs[i].name);
-    console.log('\npreview song: ' + songs[i].preview_url);
-    console.log('\nalbum: ' + songs[i].album.name);
-    console.log('\n\n\n\n');
 }
   });
 }
@@ -73,41 +82,35 @@ for (var i = 0; i < songs.length; i++) {
 
 //---------------------------------------------
 
-//Assign data being used to a variable
-var info = data.tracks.items
-// console.log(info);
+// //Assign data being used to a variable
+// var info = data.tracks.items
+// // console.log(info);
 
-//Loop through all the "items" array
-for (var i = 0; i < info.length; i++) {
-    //Store "album" object to variable
-    var albumObject = info[i].album;
-    var trackName = info[i].name
-    var preview = info[i].preview_url
-    //Store "artists" array to variable
-    var artistsInfo = albumObject.artists
-    //Loop through "artists" array
-    for (var j = 0; j < artistsInfo.length; j++) {
-        console.log("Artist: " + artistsInfo[j].name)
-        console.log("Song Name: " + trackName)
-        console.log("Preview of Song: " + preview)
-        console.log("Album Name: " + albumObject.name)
-        console.log("----------------")
-        //Append data to log.txt
-        fs.appendFileSync("log.txt", "Artist: " + artistsInfo[j].name + "\nSong Name: " + trackName + "\nPreview of Song: " + preview + "\nAlbum Name: " + albumObject.name + "\n----------------\n", function (error) {
-            if (error) {
-                console.log(error);
-            };
-        });
-    }
-}
-})
+// //Loop through all the "items" array
+// for (var i = 0; i < info.length; i++) {
+//     //Store "album" object to variable
+//     var albumObject = info[i].album;
+//     var trackName = info[i].name
+//     var preview = info[i].preview_url
+//     //Store "artists" array to variable
+//     var artistsInfo = albumObject.artists
+//     //Loop through "artists" array
+//     for (var j = 0; j < artistsInfo.length; j++) {
+//         console.log("Artist: " + artistsInfo[j].name)
+//         console.log("Song Name: " + trackName)
+//         console.log("Preview of Song: " + preview)
+//         console.log("Album Name: " + albumObject.name)
+//         console.log("----------------")
+//     }
+// }
+// })
 
 
 // -----------------------------------------
 
 
-var runThis = function(argOne, argTwo) {
-    pick(argOne, argTwo)
-};
+// var runThis = function(argOne, argTwo) {
+//     pick(argOne, argTwo)
+// };
 
-runThis(process.argv[2], process.argv[3]);
+// runThis(process.argv[2], process.argv[3]);
